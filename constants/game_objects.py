05f352faf1e47f5e8f8de19e43026714848d2788
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-01-18 09:58:05 +0200

    Added constant for Finder usage

--- constants/game_objects.py
+++ constants/game_objects.py
@@ -7,7 +7,10 @@ import gbvision as gbv
 from math import pi
 
 # 136cm diameter hoop
-UPPER_HUB = gbv.GameObject((pi * 0.68 ** 2) ** 0.5)
+UPPER_HUB_RADIUS = 0.68
+
+# Square root of the circle's area
+UPPER_HUB = gbv.GameObject((pi * UPPER_HUB_RADIUS ** 2) ** 0.5)
 
 # 24cm diameter ball
 CARGO_BALL = gbv.GameObject((pi * 0.12 ** 2) ** 0.5)
