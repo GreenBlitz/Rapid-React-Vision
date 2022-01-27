7484d9632c85247fe9ebb414dfdcc243cfb3c216
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-01-27 17:54:52 +0200

    Made local uploader for debugging

---
+++ upload_local.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -n "$1" ]
+then 
+	echo "Uploading to pi@${1}..."
+	printf "HINT: The password should be 'raspberry'.\n\n"
+	# shellcheck disable=SC2035
+	scp -r * pi@"$1":~/vision2022
+	printf "\n\nHINT: If it said 'read-only file system' then restart the RPI and try again.\n"
+else 
+	echo "No IP passed to first parameter."
+fi 
