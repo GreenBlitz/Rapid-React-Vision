ddb49dd734e1aec217f43583361fb51463ec0e1f
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-01-27 19:08:03 +0200

    :cry: you dont know how much pain this bug has caused me

--- constants/game_objects.py
+++ constants/game_objects.py
@@ -9,8 +9,9 @@ from math import pi
 # 136cm diameter hoop
 UPPER_HUB_RADIUS = 0.68
 
-# Square root of the circle's area
-UPPER_HUB = gbv.GameObject((pi * UPPER_HUB_RADIUS ** 2) ** 0.5)
+# USE THE REFLECTOR TAPE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! (i spent countless hours trying to debug this...)
+# 13cm by 5cm
+REFLECTOR_TAPE = gbv.GameObject((0.05 * 0.13) ** 0.5)
 
 # 24cm diameter ball
 CARGO_BALL = gbv.GameObject((pi * 0.12 ** 2) ** 0.5)
