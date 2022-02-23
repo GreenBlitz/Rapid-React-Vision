05719ea75e4909c6445ceb319e2e3609695f24ee
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-23 21:48:12 +0200

    Commented and added try/except

--- constants/ports.py
+++ constants/ports.py
@@ -17,15 +17,33 @@ INTERFACE_REGEX = re.compile("Interface Info:\n\tID\s+: 0x(\d+)", re.M)
 
 
 def generate_camera_ports():
+    # Init the dictionary to return
     final_dict = {}
+    # Get all the files in /dev/ which are called video* ('video' with any text after it)
     ports = glob.glob("/dev/video*")
+    # For each file found (stream)
     for port in ports:
-        pipe = sp.Popen(["v4l2-ctl", "--all", "--device", port], stdout=sp.PIPE)
-        info = (pipe.communicate()[0]).decode()
-        interface_id = INTERFACE_REGEX.findall(info)[-1]
-        if interface_id in list(INTERFACE_TO_LOC.keys()):
-            final_dict[INTERFACE_TO_LOC[interface_id]] = int(port[10:])
-        pipe.terminate()
+        try:
+            # Get all the information about this port
+            # Run the following command to get it
+            pipe = sp.Popen(["v4l2-ctl", "--all", "--device", port], stdout=sp.PIPE)
+            # Get the data that the command returned
+            # If the command does not respond within the timeout, then terminate it.
+            info = (pipe.communicate(timeout=10)[0]).decode()
+            # Close the program if it's still open for whatever reason
+            pipe.terminate()
+            # Use RegEx to get the ID of the port
+            interface_id = INTERFACE_REGEX.findall(info)[-1]
+            # If this ID is a valid camera ID
+            if interface_id in list(INTERFACE_TO_LOC.keys()):
+                # Map our temporary name to the virtual port
+                # For example, {FRONT_CAMERA : /dev/video1}
+                # What this means is that in the end, we have a dictionary where
+                # we know exactly which camera points to what file stream.
+                final_dict[INTERFACE_TO_LOC[interface_id]] = int(port[10:])
+        except sp.TimeoutExpired as e:
+            print(f"Popen failed with error: {e}")
+    # Return the output dictionary
     return final_dict
 
 LED_RING_PORT = 18
