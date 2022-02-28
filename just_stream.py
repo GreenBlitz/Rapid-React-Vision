465faa1579ff5f43964cbd58d1fc4126789d631d
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-28 21:55:12 +0200

    tried to do asaf stuff while nevo kept yelling at me

--- just_stream.py
+++ just_stream.py
@@ -1,12 +1,66 @@
+import cv2
 import gbvision as gbv
-from constants import PITCH_ANGLE, YAW_ANGLE, ROLL_ANGLE, X_OFFSET, Y_OFFSET, Z_OFFSET, TCP_STREAM_PORT
+import numpy as np
+from constants import TCP_STREAM_PORT
 
-data = gbv.LIFECAM_3000.rotate_pitch(PITCH_ANGLE). \
-    rotate_yaw(YAW_ANGLE). \
-    rotate_roll(ROLL_ANGLE). \
-    move_x(X_OFFSET). \
-    move_y(Y_OFFSET). \
-    move_z(Z_OFFSET)
+thr = gbv.ColorThreshold([[0, 100], [0, 255], [0, 255]])
 
-front_camera = gbv.USBStreamCamera(gbv.TCPStreamBroadcaster(TCP_STREAM_PORT), 0, data=data)
-front_camera.toggle_stream(True)
+front_camera = gbv.USBCamera(0)
+tcp_stream = gbv.TCPStreamBroadcaster(TCP_STREAM_PORT, max_fps=5)
+
+while True:
+	ok, image = front_camera.read()
+	# Threshold that bad boy
+	ret, mask = cv2.threshold(image[:, :, 2], 150, 255, cv2.THRESH_BINARY)
+
+	# I have literally no clue what this means
+	# God speed, stackoverflow.
+	mask_color = np.zeros_like(image)
+	mask_color[:, :, 0] = mask
+	mask_color[:, :, 1] = mask
+	mask_color[:, :, 2] = mask
+
+	# Extract the specific color range
+	my_color = cv2.bitwise_and(image, mask_color)
+	# Extract the rest of the image
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	img = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+
+	# Extract
+	gray = cv2.bitwise_and(img, 255 - my_color)
+
+	# orange masked output
+	image = image + my_color
+
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	width = int(image.shape[1] * 0.2)
+	height = int(image.shape[1] * 0.2)
+	image = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+	image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+	tcp_stream.send_frame(image)
+#
+# import cv2
+# import gbvision as gbv
+#
+# from constants import TCP_STREAM_PORT
+#
+# front_camera = gbv.USBCamera(0)
+# tcp_stream = gbv.TCPStreamBroadcaster(TCP_STREAM_PORT)
+#
+# while True:
+# 	ok, image = front_camera.read()
+#
+# 	# R
+# 	image[:, :, 2] = image[:, :, 2]
+# 	# G
+# 	image[:, :, 1] = image[:, :, 1] * 0
+# 	# B
+# 	image[:, :, 0] = image[:, :, 0] * 0
+#
+# 	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#
+# 	width = int(image.shape[1] * 0.2)
+# 	height = int(image.shape[1] * 0.2)
+# 	image = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+# 	image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+# 	tcp_stream.send_frame(image)
