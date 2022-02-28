b765bdc8029db233b1da3b4cdeca342cabed2d1d
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-28 19:52:08 +0200

    Fixed system bug

--- tools/system.py
+++ tools/system.py
@@ -7,7 +7,11 @@ def is_on_rpi() -> bool:
 
 def is_on_roborio_network() -> bool:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
+    try:
+        s.connect(("8.8.8.8", 80))
+    except OSError:
+        # No wifi, dang
+        return False
     local_ip = s.getsockname()[0]
     s.close()
     return local_ip.startswith('10.45.90.')
