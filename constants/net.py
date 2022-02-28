19762adf38a1b825e9f72d2e7c49b6d27d6f4b07
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-28 19:52:16 +0200

    why asaf ;(

--- constants/net.py
+++ constants/net.py
@@ -5,11 +5,13 @@ __is_on_roborio_network = is_on_roborio_network()
 
 TCP_STREAM_IP = '10.45.90.22' if __is_on_roborio_network else '192.168.1.8'
 
-TCP_STREAM_PORT = 1180
+TCP_STREAM_PORT = 5809
 num_of_streams = -1
+
+
 def get_stream_port():
-	global num_of_streams
-	num_of_streams += 1
-	if num_of_streams == 11:
-		raise ConnectionError("fms allows only 10 ports")
-	return TCP_STREAM_PORT+num_of_streams
+    global num_of_streams
+    num_of_streams += 1
+    if num_of_streams == 11:
+        raise ConnectionError("fms allows only 10 ports")
+    return TCP_STREAM_PORT + num_of_streams
