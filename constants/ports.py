e9bad3541bf9a02a2cc3e5032f67edac195db552
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-03-03 20:03:50 +0200

    stuff

--- constants/ports.py
+++ constants/ports.py
@@ -1,25 +1,19 @@
 """
 all ports constants go in this file
 """
-from typing import Dict
-
-from tools import is_on_rpi
-
-import subprocess as sp
-import glob
-import re
 
 DEV_CAMERA_PORT = 1
 LED_RING_PORT = 18
+#
 USB_TO_VIDEO = {
-"top right": 1,
-"top left": 3,
-"bottom right": 2,
-"bottom left": 4
+    "top right": 1,
+    "top left": 3,
+    "bottom right": 2,
+    "bottom left": 4
 }
 
+# Changes per camera location on robot
 LOC_TO_USB = {
-    "FRONT" : "bottom_right",
-    "????" : "????"
+    "FRONT": "bottom_right",
+    "????": "????"
 }
-
